f01be2dcf3c7a60e9df9c9cca98c4abb6884bb07
lining <940409607@qq.com>
2024-01-03 21:50:49 +0800

    斐波那契

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,40 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("JumpFloor(2) = " + JumpFloor(6));
+        jumpFloorII(3);
+        JumpFloorII(3);
+    }
+    //斐波那契  时间复杂度O（1）
+    public static int JumpFloor(int n){
+        if(n<=2){
+            return n;
+        }
+        int pre2 = 1, pre1 = 2;
+        int result = 0;
+        for (int i = 3;i<=n;i++){
+            result = pre1 + pre2;
+            pre2 = pre1;
+            pre1 = result;
+        }
+        return result;
+    }
+
+    public static int jumpFloorII(int target){
+        int[] dp = new int[target];
+        Arrays.fill(dp,1);
+        for (int i = 1;i<target;i++)
+            for(int j = 0; j<i ;j++)
+                dp[i] += dp[j];
+        return dp[target -1];
+
+
+    }
+    public static int JumpFloorII(int target) {
+        return (int) Math.pow(2, target - 1);
     }
 }
